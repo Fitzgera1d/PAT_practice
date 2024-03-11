7f0c4aa3c96d87f2cc1983c9cd0d0a7c4b99b7da
Fitzgera1d <gongyiqing17@alumni.nudt.edu.cn>
2024-03-12 00:42:10 +0800

    update 1022

--- advanced_level_practice/1022 Digital Library.cpp
+++ advanced_level_practice/1022 Digital Library.cpp
@@ -1,35 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct book {
-	int id;
-	string title;
-	string author;
-	set<string> keywords;
-	string publisher;
-	int year;
-};
-
 int main() {
-	int n, _tmp;
+	int n, m, _id, _tmp;
+	map<string, set<int> > libraries[6];
 	scanf("%d", &n);
-	char _str[100];
+	char str[100];
+	string _str;
 	for (int i = 0; i < n; i++) {
-		book temp;
-		scanf("%d", &_tmp);
-		temp.id = _tmp;
-		gets(_str);
-		temp.title = _str;
-		gets(_str);
-		temp.author = _str;
+		scanf("%d\n", &_id);
+		fgets(str, 100, stdin); _str = str; _str.erase(_str.end()-1);
+		libraries[1][_str].insert(_id);
+		fgets(str, 100, stdin); _str = str; _str.erase(_str.end()-1);
+		libraries[2][_str].insert(_id);
 		do {
-			scanf("%s", _str);
-			temp.keywords.insert(string(_str));
+			scanf("%s", str);
+			libraries[3][str].insert(_id);
 		} while(getchar() != '\n');
-		gets(_str);
-		temp.publisher = _str;
-		scanf("%d", &_tmp);
-		temp.year = _tmp;
+		fgets(str, 100, stdin); _str = str; _str.erase(_str.end()-1);
+		libraries[4][_str].insert(_id);
+		fgets(str, 100, stdin); _str = str; _str.erase(_str.end()-1);
+		libraries[5][_str].insert(_id);
+	}
+	scanf("%d", &m);
+	for (int i = 0; i < m; i++){
+		scanf("%d: ", &_tmp);
+		fgets(str, 100, stdin); _str = str; _str.erase(_str.end()-1);
+		printf("%d: %s\n", _tmp, _str.c_str());
+		if (libraries[_tmp].find(_str) == libraries[_tmp].end()) {
+			printf("Not Found\n");
+		} else {
+			for (auto it = libraries[_tmp][_str].begin(); it != libraries[_tmp][_str].end(); it++) {
+				printf("%07d\n", *it);
+			}
+		}
 	}
 	return 0;
 }
